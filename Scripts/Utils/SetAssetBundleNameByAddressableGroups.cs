7994b05205bafadd3ce7cd5854e699b445da3eb3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2021-12-28 06:55:15 +0700

    Add set asset bundle name tool

---
+++ Scripts/Utils/SetAssetBundleNameByAddressableGroups.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+
+public class SetAssetBundleNameByAddressableGroups : MonoBehaviour
+{
+    public AddressableAssetGroup[] groups;
+
+    [ContextMenu("Execute")]
+    public void Execute()
+    {
+        string assetPath;
+        AssetImporter assetImporter;
+        foreach (AddressableAssetGroup group in groups)
+        {
+            foreach (AddressableAssetEntry entry in group.entries)
+            {
+                assetPath = AssetDatabase.GetAssetPath(entry.MainAsset.GetInstanceID());
+                assetImporter = AssetImporter.GetAtPath(assetPath);
+                assetImporter.SetAssetBundleNameAndVariant(group.Name, "");
+                Debug.Log("-- Set " + assetPath + " asset bundle name to " + group.Name);
+            }
+        }
+    }
+}
+#endif
